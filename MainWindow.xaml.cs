f2377f3d8d8694ad7894448a2bd1cd3db21ef7cf
tnaga <hqf00342@nifty.com>
2023-07-02 13:45:30 +0900

    ListBox版追加

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,19 +1,64 @@
 ﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Shapes;
 
 namespace wpfBindingSample
 {
     public partial class MainWindow : Window
     {
+        //ドラッグ用：ドラッグ開始位置
+        private Point _dragOffset;
+
+        //ドラッグ用：ドラッグ中のRectangle。ドラッグ中でない場合はnull
+        private Rectangle? _dragRectangle;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Datagrid_CurrentCellChanged(object sender, System.EventArgs e)
+        /// <summary>
+        /// ListBox内Rectangleのマウスイベント：Down
+        /// </summary>
+        private void Rect_mouseDown(object sender, MouseButtonEventArgs e)
         {
-            //セル編集したら即時反映
-            //通常は別行へフォーカスが移らないと反映されない
-            datagrid.CommitEdit();
+            _dragRectangle = sender as Rectangle;
+            if (_dragRectangle != null)
+            {
+                //Rectangleをキャプチャしドラッグ開始
+                _dragRectangle.CaptureMouse();
+
+                //Rectangle内のどこをクリックされたか記憶
+                _dragOffset = e.GetPosition(_dragRectangle);
+                this.Cursor = Cursors.Hand;
+            }
         }
+
+        /// <summary>
+        /// ListBox内Rectangleのマウスイベント：Move
+        /// </summary>
+        private void Rect_mouseMove(object sender, MouseEventArgs e)
+        {
+            if (_dragRectangle != null && listbox.SelectedItem is RectInfo rinfo)
+            {
+                //RectinfoのX/Yプロパティを変更。
+                var pos = e.GetPosition(listbox);
+                rinfo.X = (int)(pos.X - _dragOffset.X);
+                rinfo.Y = (int)(pos.Y - _dragOffset.Y);
+            }
+        }
+
+        /// <summary>
+        /// ListBox内Rectangleのマウスイベント：Up
+        /// </summary>
+        private void Rect_mouseUp(object sender, MouseButtonEventArgs e)
+        {
+            //Dragを開放
+            _dragRectangle = null;
+            Mouse.Capture(null);
+            this.Cursor = Cursors.Arrow;
+        }
+
+        private void Datagrid_CurrentCellChanged(object sender, System.EventArgs e) => datagrid.CommitEdit();
     }
 }
