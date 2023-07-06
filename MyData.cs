8eed3e9487d4d7f351b915718278e1ef55c27ac4
tnaga <hqf00342@nifty.com>
2023-07-06 13:00:58 +0900

    ModelとViewModelを混ぜていたのを分離。MyDataクラスを追加

---
+++ MyData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+
+namespace wpfBindingSample;
+
+/// <summary>
+/// アプリ内のデータを管理するモデル
+/// シングルトンのため、static classにしている
+/// </summary>
+public static class MyData
+{
+    /// <summary>RectInfoのコレクション</summary>
+    public static ObservableCollection<RectInfo> RectInfos { get; set; } = new();
+
+    static MyData()
+    {
+        RectInfos.Add(new RectInfo("1", 10, 10));
+        RectInfos.Add(new RectInfo("2", 200, 10));
+        RectInfos.Add(new RectInfo("3", 10, 200));
+        RectInfos.Add(new RectInfo("4", 200, 200));
+    }
+
+    public static void DeleteItem(RectInfo r)
+    {
+        RectInfos.Remove(r);
+    }
+}
--- ViewModel.cs
+++ ViewModel.cs
@@ -3,28 +3,26 @@ using System.Collections.ObjectModel;
 
 namespace wpfBindingSample;
 
+/// <summary>
+/// MainWindow用Viewモデル。
+/// モデルのデータはシングルトンのMyDataを参照している。
+/// </summary>
 public class ViewModel : BindableBase
 {
     /// <summary>
     /// Rectangleコレクション。
-    /// 本来はINotifyPropertyChanged実装すべきだがこのサンプルでは変更されないので
-    /// getterのみの通常プロパティとしている</summary>
-    public ObservableCollection<RectInfo> RectInfoCollection { get; } = new();
+    /// </summary>
+    private ObservableCollection<RectInfo> _rectInfoCollection = null!;
+    public ObservableCollection<RectInfo> RectInfoCollection { get => _rectInfoCollection; set => SetProperty(ref _rectInfoCollection, value); }
+
 
     /// <summary>アイテム削除コマンド</summary>
     private DelegateCommand? _delCmd;
 
-    public DelegateCommand DeleteItemCommand => _delCmd ??= new(o => { if (o is RectInfo item) RectInfoCollection.Remove(item); });
+    public DelegateCommand DeleteItemCommand => _delCmd ??= new(o => { if (o is RectInfo item) MyData.DeleteItem(item); });
 
     public ViewModel()
     {
-        //デモ用に4つのRectangleを設置
-        RectInfoCollection = new()
-        {
-            new RectInfo("1", 10, 10),
-            new RectInfo("2", 200, 10),
-            new RectInfo("3", 10, 200),
-            new RectInfo("4", 200, 200)
-        };
+        RectInfoCollection = MyData.RectInfos;
     }
 }
